674f97bbbdf0b82496efe439b43aabb83d0df67b
Tu Nombre <hersilveira@gmail.com>
2026-09-05 00:10:49 +0200

    Fix GitHub Pages asset base path

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
+export default defineConfig({ base: './',
   plugins: [{
     name: 'inject-paddleocr-module',
     transformIndexHtml(html) {
