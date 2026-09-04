594257de9b8e86ca653ab3bb31507da25ae18b26
Tu Nombre <hersilveira@gmail.com>
2026-09-05 00:08:01 +0200

    Refresh GitHub Pages service worker cache

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,4 @@
-const CACHE='ledger-v1';
+const CACHE='ledger-v2';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./styles.css','./app.js','./manifest.json']))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html')))));
