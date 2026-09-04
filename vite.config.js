03ef08d28d507700709290de8ee922d64863d140
Tu Nombre <hersilveira@gmail.com>
2026-09-04 23:30:25 +0200

    Fix GitHub Pages static asset paths

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   plugins: [{
     name: 'inject-paddleocr-module',
     transformIndexHtml(html) {
-      return html.replace('<script src="app.js?v=2"></script>', '<script type="module" src="/paddle-ocr.js"></script><script src="app.js?v=2"></script>');
+      return html.replace('<script src="app.js?v=2"></script>', '<script type="module" src="./paddle-ocr.js"></script><script src="./app.js?v=2"></script>');
     }
   }]
 });
